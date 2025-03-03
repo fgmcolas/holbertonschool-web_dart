04725628031247ecc75a8d876af74b61467fddb7
fgmcolas <https://github.com/fgmcolas>
2025-03-03 16:59:47 +0100

    Task 4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,53 +1,19 @@
-import "4-util.dart";
-import "dart:convert";
+import '4-util.dart';
+import 'dart:convert';
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
   try {
-    String userData = await fetchUserData();
-    Map<String, dynamic> userJson = json.decode(userData);
-    String userId = userJson["id"];
+    Map userData = jsonDecode(await fetchUserData());
+    String userId = userData['id'];
 
-    String ordersData = await fetchUserOrders(userId);
-    if (ordersData == "null" || ordersData.isEmpty) {
-      return 0.0;
+    List<dynamic> userOrder = jsonDecode(await fetchUserOrders(userId));
+
+    double total = 0.0;
+    for (var product in userOrder) {
+      total += double.parse(await fetchProductPrice(product));
     }
-
-    List<dynamic>? products;
-    try {
-      products = json.decode(ordersData);
-    } catch (e) {
-      return -1;
-    }
-
-    if (products == null || products.isEmpty) {
-      return 0.0;
-    }
-
-    List<Future<String>> priceFutures = products.map((product) => fetchProductPrice(product)).toList();
-    List<String> pricesData = await Future.wait(priceFutures);
-
-    double totalPrice = 0.0;
-
-    for (int i = 0; i < pricesData.length; i++) {
-      String price = pricesData[i];
-      if (price == "null" || price.isEmpty) {
-        continue;
-      }
-
-      double? parsedPrice;
-      try {
-        parsedPrice = json.decode(price);
-      } catch (e) {
-        return -1;
-      }
-
-      if (parsedPrice != null) {
-        totalPrice += parsedPrice;
-      }
-    }
-
-    return totalPrice;
-  } catch (e) {
+    return total;
+  } catch (error) {
     return -1;
   }
 }
