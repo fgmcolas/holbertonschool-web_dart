6bb0cf7725b244fd6072383382d16f9bddf04392
fgmcolas <https://github.com/fgmcolas>
2025-03-03 17:01:02 +0100

    Task 5

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,16 +1,24 @@
-import "package:http/http.dart" as http;
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-printRmCharacters() async {
+Future<void> printRmCharacters() async {
   try {
-    final res = await http.get(
-      Uri.parse('https://rickandmortyapi.com/api/character'),
-    );
-    var json = jsonDecode(res.body);
-    for (var idx = 0; idx < json['results'].length; idx++) {
-      print("${json['results'][idx]['name']}");
+    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      if (data['results'] is List) {
+        for (var character in data['results']) {
+          print('${character['name']}');
+        }
+      } else {
+        print('Invalid API response format.');
+      }
+    } else {
+      print('Failed to fetch data. Status code: ${response.statusCode}');
     }
-  } catch (err) {
-    print('error caught: $err');
+  } catch (error) {
+    print('Error caught: $error');
   }
 }
