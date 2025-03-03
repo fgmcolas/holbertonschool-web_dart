de6c96aa51ed982f4c176650bcb5aac6f92f4ef6
fgmcolas <https://github.com/fgmcolas>
2025-03-03 16:55:15 +0100

    Task 4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,47 +1,34 @@
-import 'dart:convert';
-import 'dart:async';
-import 'dart:core';
+import "4-util.dart";
+import "dart:convert";
 
-Future<String> fetchUserOrders(String id) async {
-  var orders = {
-    "7ee9a243-01ca-47c9-aa14-0149789764c3": ["pizza", "orange"]
-  };
+Future<double> calculateTotal() async {
   try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(orders[id]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<String> fetchProductPrice(product) async {
-  var products = {"pizza": 20.30, "orange": 10, "water": 5, "soda": 8.5};
-  try {
-    return Future.delayed(
-        const Duration(seconds: 2), () => json.encode(products[product]));
-  } catch (err) {
-    return "error caught : $err";
-  }
-}
-
-calculateTotal() async {
-    try {
-        double price = 0;
-        final Map<String, dynamic> userData = json.decode(await fetchUserData());
-        final String data = userData['id'];
-        final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
-        for (int idx = 0; idx < userOrder.length; idx++) {
-            price += json.decode(await fetchProductPrice(userOrder[idx]));
-        }
-        return price;
-    } catch (err) {
-        print('error caught: $err');
-        return -1;
+    String userData = await fetchUserData();
+    Map<String, dynamic> userJson = json.decode(userData);
+    String userId = userJson["id"];
+    String ordersData = await fetchUserOrders(userId);
+    if (ordersData == "null" || ordersData.isEmpty) {
+      return 0.0;
     }
+
+    List<dynamic> products = json.decode(ordersData);
+    if (products.isEmpty) {
+      return 0.0;
+    }
+
+    List<Future<String>> priceFutures = products.map((product) => fetchProductPrice(product)).toList();
+    List<String> pricesData = await Future.wait(priceFutures);
+
+    double totalPrice = 0.0;
+    for (String price in pricesData) {
+      if (price == "null" || price.isEmpty) {
+        continue;
+      }
+      totalPrice += json.decode(price) ?? 0.0;
+    }
+
+    return totalPrice;
+  } catch (e) {
+    return -1;
+  }
 }
